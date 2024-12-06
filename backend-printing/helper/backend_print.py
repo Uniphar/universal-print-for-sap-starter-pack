f9edf2edfd3bf1f4339314b7c40b098adc19ed83
VaclavK <vaclav@vaclavkabat.net>
2024-12-06 16:52:00 +0000

    test3

--- backend-printing/helper/backend_print.py
+++ backend-printing/helper/backend_print.py
@@ -181,7 +181,7 @@ class BackendPrint:
                         )
                         self.logger.info(
                             f"[{self.log_tag}] Fetched {len(print_items_from_queue)} items from the "
-                            + f" SAP queue for {sap_system.sap_sid}"
+                            + f" SAP queue  {queue} for {sap_system.sap_sid}"
                         )
                         for print_item in print_items_from_queue:
                             queue_item_params = json.loads(print_item["QItemParams"])
